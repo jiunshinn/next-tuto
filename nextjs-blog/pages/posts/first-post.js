c205f7e604a896920e9f29a37a9eeb33c6887af0
jiun-dev <jiun.dev@gmail.com>
2022-04-16 11:40:56 +0900

    head

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Nest.js</h1>
             <h2>
                 <Link href="/">
